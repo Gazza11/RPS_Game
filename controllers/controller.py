19e1a1d4ccfd6a471a025a15d2f2528c400eaae6
Gary Darnell <gdarnell9@gmail.com>
2021-05-29 15:39:43 +0100

    Basic css added to header of base.

--- controllers/controller.py
+++ controllers/controller.py
@@ -8,17 +8,21 @@ self = 'self'
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Home', players=players)
+    return render_template('welcome.html', title='Home', players=players)
 
 @app.route('/<input_1>/<input_2>', methods=['GET', 'POST'])
 def game_r_s(input_1, input_2):
-    players[0] = Player('Holder1', input_1)
-    players[1] = Player('Holder2', input_2)
-    player1 = Player('Holder1', input_1)
-    player2 = Player('Holder2', input_2)
+    players[0] = Player('Player 1', input_1)
+    players[1] = Player('Player 2', input_2)
+    player1 = Player('Player 1', input_1)
+    player2 = Player('Player 2', input_2)
     result = Game.game_run(self, player1, player2)
     return render_template('index.html', title='Home', players = players, result=result)
 
 @app.route('/welcome')
 def welcome():
     return render_template('welcome.html', title='Welcome')
+
+@app.route('/play')
+def play():
+    return render_template('index.html', title='play', players=players)

0d960c1afe3ba2fa48735f76ec0e3fe4d4f3b010
Gary Darnell <gdarnell9@gmail.com>
2021-05-29 16:37:54 +0100

    ADV extensions done. Now able to play the computer.

--- controllers/controller.py
+++ controllers/controller.py
@@ -24,5 +24,35 @@ def welcome():
     return render_template('welcome.html', title='Welcome')
 
 @app.route('/play')
+def play_hold():
+    return render_template('play.html', title='Play', players=players)
+
+@app.route('/play', methods=['POST'])
 def play():
-    return render_template('index.html', title='play', players=players)
+    player1_name = request.form["player1-name"]
+    player2_name = request.form['player2-name']
+    player1_choice = request.form['player1-choice']
+    player2_choice = request.form['player2-choice']
+    players[0] = Player(player1_name, player1_choice)
+    players[1] = Player(player2_name, player2_choice)
+    player1 = Player(player1_name, player1_choice)
+    player2 = Player(player2_name, player2_choice)
+    result = Game.game_run(self, player1, player2)
+    return render_template('index.html', title='play', players=players, result=result)
+
+
+@app.route('/play-cpu')
+def cpu_hold():
+    return render_template('cpu.html', title='Play the CPU')
+
+@app.route('/play-cpu', methods=['POST'])
+def play_cpu():
+    computer = Player.computer()
+    player1_name = request.form["player1-name"]
+    player1_choice = request.form['player1-choice']
+    players[0] = Player(player1_name, player1_choice)
+    players[1] = computer
+    player1 = Player(player1_name, player1_choice)
+    player2 = computer
+    result = Game.game_run(self, player1, player2)
+    return render_template('index.html', title='play', players=players, result=result)
--- models/player.py
+++ models/player.py
@@ -4,3 +4,11 @@ class Player:
     def __init__(self, name, choice):   
         self.name = name 
         self.choice = choice
+
+
+    def computer():
+        possible_choices = ['rock', 'paper', 'scissors']
+        index = random.randint(0,2)
+        choice = possible_choices[index]
+        computer_file = Player('Computer', choice)
+        return computer_file
